8fb41d1644d4a271ea485772cc562350a339e5d0
Felipe R. Tasoniero <frtasoniero@gmail.com>
2024-12-15 13:32:10 -0300

    finish part one from day three

--- aoc2024/daythree/daythree.go
+++ aoc2024/daythree/daythree.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 )
 
 func Run() error {
@@ -18,15 +19,27 @@ func Run() error {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	regex := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
-	// var total float64 = 0.0
+	regexWords := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+	regexNumbers := regexp.MustCompile("[0-9]+")
+	var multiplicationsResult float64 = 0.0
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		matches := regex.FindAllString(line, -1)
+		matches := regexWords.FindAllString(line, -1)
 		for _, match := range matches {
-			fmt.Println("Found match:", match)
+			numbers := regexNumbers.FindAllString(match, -1)
+
+			num1, err := strconv.Atoi(numbers[0])
+			if err != nil {
+				return fmt.Errorf("error converting number: %w", err)
+			}
+			num2, err := strconv.Atoi(numbers[1])
+			if err != nil {
+				return fmt.Errorf("error converting number: %w", err)
+			}
+
+			multiplicationsResult = multiplicationsResult + (float64(num1) * float64(num2))
 		}
 	}
 
@@ -34,6 +47,8 @@ func Run() error {
 		fmt.Println("Error reading file:", err)
 		return err
 	}
+
+	fmt.Println("Multiplications result:", multiplicationsResult)
 
 	return nil
 }
